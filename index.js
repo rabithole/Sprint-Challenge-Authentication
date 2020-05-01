a28590383ce4a01403518b30fd8d71adad930249
Robert Kulp <pedalinon@gmail.com>
2020-05-01 11:39:47 -0700

    pre loaded

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-const server = require('./server.js');
+const server = require('./api/server.js');
 
-const port = process.env.PORT || 5000;
-server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
+const PORT = process.env.PORT || 3300;
+server.listen(PORT, () => {
+  console.log(`\n=== Server listening on port ${PORT} ===\n`);
+});
