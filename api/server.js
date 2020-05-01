b8de015ea94f77df47e2430f45d9ded8e0b3077a
Robert Kulp <pedalinon@gmail.com>
2020-05-01 14:08:18 -0700

    things are happening

--- api/server.js
+++ api/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
+const bcrypt = require('bcryptjs');
 
 const authenticate = require('../auth/authenticate-middleware.js');
 const authRouter = require('../auth/auth-router.js');
@@ -11,6 +12,10 @@ const server = express();
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
+
+server.get("/", (req, res) => {
+  res.json({ api: "up, down, up, down" });
+});
 
 server.use('/api/auth', authRouter);
 server.use('/api/jokes', authenticate, jokesRouter);
--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,48 @@
 const router = require('express').Router();
+const bcrypt = require('bcryptjs');
+
+const Users = require('../user/user-model.js');
 
 router.post('/register', (req, res) => {
   // implement registration
+  const user = req.body;
+    // console.log('register')
+    // this takes the req.body and creates the hash
+    const hash = bcrypt.hashSync(user.password, 8);
+    // this takes the user password and assigns it to the hash. AKA, replaces the password with the hash. 
+    user.password = hash;
+
+    Users.add(user)
+        .then(saved => {
+            res.status(201).json(saved);
+        })
+        .catch(error => {
+            res.status(500).json(error);
+        });
 });
 
 router.post('/login', (req, res) => {
-  // implement login
+    let { username, password } = req.body;
+    // console.log('login')
+
+    Users.findBy({ username })
+        .first()
+        .then(user => {
+            if (user && bcrypt.compareSync(password, user.password)) {
+                // const token = genToken(user);
+                // req.session.user = username;
+                res.status(200).json({ 
+                    message: `Welcome ${user.username}!`,
+                    token,  
+                });
+            } else {
+                res.status(401).json({ message: 'You shall not pass!' });
+            }
+        })
+        .catch(error => {
+           
+            res.status(500).json(error);
+        });
 });
 
 module.exports = router;
