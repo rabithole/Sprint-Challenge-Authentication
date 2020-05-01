740952a8ed365d318f6a2062b496783734c93590
Robert Kulp <pedalinon@gmail.com>
2020-05-01 07:52:21 -0700

    initialize

---
+++ Self-study-questions.md
@@ -0,0 +1,11 @@
+### What is the purpose of using sessions?
+    - Sessions are used to save authentication data so it won't be necessary to re-enter credientials with every request. 
+
+### What does bcrypt do to help us store passwords in a secure manner.
+    - It hashes the password using an algorythm multiplied by a designated number of times. 
+
+### What does bcrypt do to slow down attackers?
+    - Bcrypt uses a hash function to transform a string into another string to conceal a password. It does this a multiple of times determined by the programmer. 
+
+### What are the three parts of the JSON Web Token?
+    - Header, payload and signature. 
---
+++ server.js
@@ -0,0 +1,74 @@
+const express = require("express");
+// const helmet = require("helmet");
+// const cors = require("cors");
+
+// const bcrypt = require('bcryptjs');
+// const session = require('express-session');
+// const restricted = require('./auth/restricted-middleware.js');
+
+// passing the session object above to the HOC below. 
+// const knexSessionStore = require('connect-session-knex')(session);
+// const jwt = require('jsonwebtoken');
+
+const usersRouter = require("./router/user-router.js");
+const authRouter = require("./auth/auth-router.js");
+
+const server = express();
+
+const sessionConfig = {
+  name: 'logged-in-user',
+  secret: 'myspeshulsecret',
+  cookie: {
+    maxAge: 3600 * 1000,
+    secure: false, // should be true in production
+    httpOnly: true
+  },
+  resave: false,
+  saveUninitialized: false,
+
+  // adds logged in persistance
+  store: new knexSessionStore(
+    {
+      knex: require("./data/dbConfig.js"),
+      tablename: "sessions",
+      sidfieldname: "sid",
+      createtable: true,
+      clearInterval: 3600 * 1000
+    }
+  )
+}
+
+// global middleware
+// server.use(helmet());
+server.use(express.json());
+// server.use(cors());
+server.use(session(sessionConfig));
+
+server.use(session(sessionConfig));
+
+server.use("/api/user", restricted, usersRouter);
+server.use("/api/auth", authRouter);
+
+server.get("/", (req, res) => {
+  res.json({ api: "up, down, up, down" });
+});
+
+server.get('/token', (req, res) => {
+  const payload = {
+    subject: 'Robert', 
+    userid: 'ROK', 
+    favoriteBike: 'mine'
+  }
+
+  const secret = 'thesecret';
+
+  const options = {
+    expiresIn: '30m'
+  }
+
+  const token = jwt.sign(payload, secret, options);
+
+  res.json(token);
+})
+
+module.exports = server;
